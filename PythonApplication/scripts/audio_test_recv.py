9b19395b4a247d3acfb0107e2ace2c5d3cc7e9cf
William Dale <wd40bug@localhost.localdomain>
2026-04-01 16:54:52 -0500

    Audio test working over radio!!!

--- PythonApplication/scripts/audio_test_recv.py
+++ PythonApplication/scripts/audio_test_recv.py
@@ -1,12 +1,22 @@
+import threading
 from rtty_sdr.core.protocol import ProtocolDebug, RecvMessage, SendMessage, protocol
 from rtty_sdr.debug.annotations import DebugAnnotations
 from rtty_sdr.debug.squelch import plot_shaded_squelch
 from rtty_sdr.debug.state_changes import graph_states
 from rtty_sdr.dsp.decode import decode_stream
 from rtty_sdr.dsp.engines import EnvelopeEngine, GoertzelEngine
+from rtty_sdr.dsp.poisonPill import PillQueue, QueuePoisonPill
 from rtty_sdr.dsp.squelch import Squelch
 from rtty_sdr.dsp.sources import MockSignalSource
-from rtty_sdr.core.options import SystemOpts, RTTYOpts
+from rtty_sdr.core.options import (
+    DecodeCommon,
+    GoertzelOpts,
+    SignalOpts,
+    SquelchOpts,
+    SystemOpts,
+    RTTYOpts,
+    DecodeStreamOpts,
+)
 from rtty_sdr.debug.awgn import awgn
 from rtty_sdr.debug.internal_signal import internal_signal
 from rtty_sdr.core.baudot import BaudotDecoder, BaudotEncoder
@@ -16,35 +26,64 @@ import numpy.typing as npt
 import matplotlib.pyplot as plt
 import sys
 from rtty_sdr.dsp.sources import MicrophoneSource
+import queue
 
 Fs = 8000
 rtty = RTTYOpts(baud=45.45, mark=2125, shift=170, pre_msg_stops=1, stop_bits=2)
-opts = SystemOpts(Fs, rtty)
+opts = SignalOpts(Fs=Fs, rtty=rtty)
 
 chunk_size = opts.nsamp // 5
 overlap_size = chunk_size // 2
 
-source = MicrophoneSource(Fs, chunk_size)
-engine = GoertzelEngine(overlap_size, 256, opts)
+decode = DecodeCommon(oversampling=5, signal=opts)
+source = MicrophoneSource(decode)
+engine = GoertzelEngine(GoertzelOpts(overlap_ratio=0.5, dft_len=256, decode=decode))
 
 annotations = DebugAnnotations(np.array([]), np.array([]), np.array([]))
 envelope: npt.NDArray[np.float64] = np.array([])
 indices: npt.NDArray[np.int_] = np.array([])
 
-squelch = Squelch(opts, 0.2, 1)
+squelch = Squelch(SquelchOpts(lower_thresh=0.2, upper_thresh=1, decode=decode))
 decoder = BaudotDecoder()
 
-generator = decode_stream(source, squelch, engine, opts, chunk_size // 4, chunk_size * 2)
+num_msgs = 2
 
+pill_queue: PillQueue = queue.Queue()
+t = threading.Timer(10, lambda: pill_queue.put("stop"))
+
+generator = decode_stream(
+    source,
+    squelch,
+    engine,
+    DecodeStreamOpts(
+        squelch_grace_percent=0.25, idle_bits=2, none_friction=0.2, decode=decode
+    ),
+    QueuePoisonPill(pill_queue),
+)
+
+num_received = 0
+
+t.start()
+
+messages_received: list[RecvMessage | ProtocolDebug] = []
 for received in protocol(generator, decoder):
+    messages_received.append(received)
+    t.cancel()
+    t = threading.Timer(10, lambda: pill_queue.put("stop"))
+    t.start()
+
+for received in messages_received:
     debug: ProtocolDebug
     if isinstance(received, RecvMessage):
         print(f"Received: {received.encoding}")
         debug = received.debug
+        num_received += 1
+        if num_received == num_msgs:
+            pill_queue.put("stop")
     else:
         debug = received
 
-    fig, axs = plt.subplots(3,1)
+    fig, axs = plt.subplots(3, 1)
     local_t = debug.decode.indices / Fs
     axs[0].plot(local_t, debug.decode.envelope)
     if isinstance(received, RecvMessage):
@@ -56,10 +95,10 @@ for received in protocol(generator, decoder):
     axs[1].plot(local_t, debug.decode.envelope)
     axs[1].set_title("With ProtocolState")
     graph_states(local_t, axs[1], debug.states)
-    axs[1].legend(bbox_to_anchor=(1.00, 0.5), loc='center left', borderaxespad=0.)
+    axs[1].legend(bbox_to_anchor=(1.00, 0.5), loc="center left", borderaxespad=0.0)
 
     axs[2].plot(local_t, debug.decode.envelope)
     axs[2].set_title("With Squelch")
     plot_shaded_squelch(local_t, fig.axes[2], debug.decode.squelch)
-    axs[2].legend(bbox_to_anchor=(1.00, 0.5), loc='center left', borderaxespad=0.)
+    axs[2].legend(bbox_to_anchor=(1.00, 0.5), loc="center left", borderaxespad=0.0)
     plt.show()
